b17651598bceba850702296596271223a75aedf6
codeworksforkevy <simreyalcin@yahoo.com>
2026-02-09 18:00:59 +0300

    Create json_utils.py

---
+++ utils/json_utils.py
@@ -0,0 +1,9 @@
+# utils/json_utils.py
+import json
+import os
+
+def load_json(path):
+    if not os.path.exists(path):
+        return {}
+    with open(path, "r", encoding="utf-8") as f:
+        return json.load(f)
