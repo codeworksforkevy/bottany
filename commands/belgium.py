191093c5db9d84b83ca9e3d86b667928f1fe962f
codeworksforkevy <simreyalcin@yahoo.com>
2026-02-13 20:19:14 +0300

    Update belgium.py

--- commands/belgium.py
+++ commands/belgium.py
@@ -1,5 +1,17 @@
+import discord
+from discord import app_commands
 
-from commands.belgian_chocolate import register_belgium_chocolate
+class Belgium(app_commands.Group):
+    def __init__(self):
+        super().__init__(name="belgium", description="Belgium related commands")
+
+    @app_commands.command(name="beverages")
+    async def beverages(self, interaction: discord.Interaction):
+        await interaction.response.send_message("Belgian beverages")
+
+    @app_commands.command(name="chocolate")
+    async def chocolate(self, interaction: discord.Interaction):
+        await interaction.response.send_message("Belgian chocolate")
 
 def register_belgium(client, tree, data_dir):
-    return register_belgium_chocolate(client, data_dir)
+    tree.add_command(Belgium())
