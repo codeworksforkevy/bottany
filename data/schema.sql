192d5f0fcfb31b8efec9e7013e09db19c7962173
codeworksforkevy <simreyalcin@yahoo.com>
2026-02-26 00:36:50 +0300

    Update schema.sql

--- data/schema.sql
+++ data/schema.sql
@@ -1,9 +1,12 @@
-
 CREATE TABLE IF NOT EXISTS stream_snapshots (
     id SERIAL PRIMARY KEY,
-    user_login TEXT,
+    user_login TEXT NOT NULL,
     viewer_count INT,
     title TEXT,
     game_name TEXT,
+    started_at TIMESTAMP,
     recorded_at TIMESTAMP DEFAULT NOW()
 );
+
+CREATE INDEX IF NOT EXISTS idx_stream_user_time
+ON stream_snapshots(user_login, recorded_at DESC);
