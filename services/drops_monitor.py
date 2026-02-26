691af9bdbb7f4156e184af21d3e4718693620900
codeworksforkevy <simreyalcin@yahoo.com>
2026-02-26 14:23:43 +0300

    Update drops_monitor.py

--- services/drops_monitor.py
+++ services/drops_monitor.py
@@ -1,53 +1,35 @@
-import time
 from services.diff_engine import generate_hash
-
-DROPS_TTL = 300
 
 
 class DropsLifecycleMonitor:
 
-    def __init__(self, api, cache, logger):
+    def __init__(self, api, cache, logger, telemetry):
         self.api = api
         self.cache = cache
         self.logger = logger
+        self.telemetry = telemetry
 
-    async def check_game(self, game_name: str):
+    async def check_game(self, game_name):
 
         key = f"drops_{game_name}"
 
-        # Streams çek
-        data = await self.api.get(
-            "streams",
-            params={"game_name": game_name, "first": 20}
-        )
-
+        data = await self.api.get("streams", params={"game_name": game_name})
         streams = data.get("data", [])
 
-        drops_detected = any(
-            "drops" in (stream.get("title", "").lower())
-            for stream in streams
-        )
+        drops_active = any("drops" in s.get("title", "").lower() for s in streams)
 
-        new_state = {
+        comparison_state = {
             "game": game_name,
-            "drops_active": drops_detected,
-            "timestamp": time.time()
+            "drops_active": drops_active
         }
 
-        old_state = self.cache.get(key)
+        previous = self.cache.get(key)
 
-        if not old_state:
-            self.cache.set(key, new_state, DROPS_TTL)
+        if previous and generate_hash(previous) == generate_hash(comparison_state):
             return
 
-        if generate_hash(old_state) != generate_hash(new_state):
-            self.logger.log(
-                "drops_state_changed",
-                {
-                    "game": game_name,
-                    "old": old_state,
-                    "new": new_state
-                }
-            )
+        self.cache.set(key, comparison_state, ttl=300)
 
-        self.cache.set(key, new_state, DROPS_TTL)
+        await self.telemetry.log_drops_state(game_name, drops_active)
+
+        self.logger.log("drops_state_changed", comparison_state)
