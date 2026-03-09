482c056899e87d42f26a5e2fe467a071a2c4ae4c
codeworksforkevy <simreyalcin@yahoo.com>
2026-03-10 00:19:08 +0300

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,21 @@
+-- =================================================
+-- PET A KITTEN TABLE
+-- =================================================
+
 CREATE TABLE IF NOT EXISTS kitten_pets (
+
     guild_id BIGINT NOT NULL,
     user_id BIGINT NOT NULL,
+
+    -- how many times the user used /petakitten
     pet_count INTEGER NOT NULL DEFAULT 0,
+
+    -- tier name
     tier TEXT,
+
+    -- unlocked achievements
+    achievements TEXT[] DEFAULT ARRAY[]::TEXT[],
+
     PRIMARY KEY (guild_id, user_id)
+
 );
