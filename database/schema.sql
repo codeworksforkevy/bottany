e08dd8a7567df3f0e59d31106fa9c7d7e70a3015
codeworksforkevy <simreyalcin@yahoo.com>
2026-03-10 00:11:29 +0300

    Update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS kitten_pets (
     guild_id BIGINT NOT NULL,
     user_id BIGINT NOT NULL,
-    pet_count INTEGER DEFAULT 0,
-    tier TEXT DEFAULT 'None',
+    pet_count INTEGER NOT NULL DEFAULT 0,
+    tier TEXT,
     PRIMARY KEY (guild_id, user_id)
 );
